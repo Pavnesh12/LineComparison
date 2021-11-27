b386f07f7c8b5828b7a4a6369c686a721ca287f2
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-11-27 22:00:56 +0530

    find the length of the line

---
+++ LineCompare/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line comparison computation program");
+            Class1 c = new Class1();
+            c.length();
+        }
+    }
+}
