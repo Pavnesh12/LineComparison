8759a5f6ee375c858b403dc63a89e5c4d8af0c9b
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-11-27 21:07:48 +0530

    welcome to line comparison problem

---
+++ LineCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line comparison computation program");
+        }
+    }
+}
