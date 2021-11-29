53b0b6a2d3fe8597b1457bb07512c4980e557167
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-11-29 21:36:25 +0530

    two lines compare

--- LineCompare/Class1.cs
+++ LineCompare/Class1.cs
@@ -7,9 +7,8 @@ namespace LineCompare
     internal class Class1
     {
         int x1, x2, y1, y2;
-       double lengthc = 0;
-
-    public void length()
+       double l1 = 0 , l2= 0;
+      public double line1()
         {
             Console.WriteLine("enter the co-ordinates x1,y1,x2,y2");
             x1 = Convert.ToInt32(Console.ReadLine());
@@ -18,11 +17,24 @@ namespace LineCompare
             y2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("x1,y1" + " = "+ x1 + "," + y1);
             Console.WriteLine("x2,y2" + " = "+x2 + "," + y2);
-            lengthc = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
-            Console.WriteLine(lengthc);
+            l1 = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+            Console.WriteLine(l1);
+            return l1;
 
         }
+        public double line2()
+        {
+            Console.WriteLine("enter the co-ordinates x1,y1,x2,y2");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x1,y1" + " = " + x1 + "," + y1);
+            Console.WriteLine("x2,y2" + " = " + x2 + "," + y2);
+            l2 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
 
+            return l2;
+        }
 
     }
 }
--- LineCompare/Program.cs
+++ LineCompare/Program.cs
@@ -6,9 +6,22 @@ namespace LineCompare
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome to line comparison computation program");
+            double p1=0, p2=0;
             Console.WriteLine("line comparison computation program");
             Class1 c = new Class1();
-            c.length();
+          p1 = c.line1();
+          p2 = c.line2();
+            if (p1 == p2)
+            {
+                Console.WriteLine("Both line are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both line are not equal");
+            }
+
+
         }
     }
 }
